ff516661e37cd5e379ffac5739746fc626edd6ec
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-09 16:44:20 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.google.gms.google.services) apply false
-    alias(libs.plugins.google.firebase.crashlytics) apply false
-    alias(libs.plugins.google.firebase.firebase.perf) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     id("androidx.room") version "2.8.4" apply false
