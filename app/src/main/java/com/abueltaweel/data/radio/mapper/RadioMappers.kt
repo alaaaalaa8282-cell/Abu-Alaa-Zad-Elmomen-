b8265b68dc654b56c6fa831d3b8db97233149d04
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-10 19:03:03 +0300

    Add files via upload

--- app/src/main/java/com/abueltaweel/data/radio/mapper/RadioMappers.kt
+++ app/src/main/java/com/abueltaweel/data/radio/mapper/RadioMappers.kt
@@ -4,17 +4,12 @@ import com.abueltaweel.data.radio.dto.CategoryDto
 import com.abueltaweel.data.radio.dto.RadioChannelDto
 import com.abueltaweel.domain.entity.radio.Category
 import com.abueltaweel.domain.entity.radio.RadioChannel
-import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
-
-@OptIn(ExperimentalTime::class)
 fun RadioChannelDto.toDomain(): RadioChannel {
     return RadioChannel(
         id = this.id,
         nameAr = this.nameAr,
         nameEn = this.nameEn,
         streamUrl = this.streamUrl,
-        createdAt = Instant.parse(this.createdAt),
         categoryId = this.categoryId
     )
 }
