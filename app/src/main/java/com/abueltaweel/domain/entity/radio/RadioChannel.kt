9f9d41e7846e586167583d406680ee8f16e6508f
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-10 19:04:05 +0300

    Add files via upload

--- app/src/main/java/com/abueltaweel/domain/entity/radio/RadioChannel.kt
+++ app/src/main/java/com/abueltaweel/domain/entity/radio/RadioChannel.kt
@@ -1,14 +1,8 @@
 package com.abueltaweel.domain.entity.radio
-
-import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
-
-@OptIn(ExperimentalTime::class)
 data class RadioChannel(
     val id: Int,
     val nameAr: String,
     val nameEn: String,
     val streamUrl: String,
-    val createdAt: Instant,
     val categoryId: String
 )
