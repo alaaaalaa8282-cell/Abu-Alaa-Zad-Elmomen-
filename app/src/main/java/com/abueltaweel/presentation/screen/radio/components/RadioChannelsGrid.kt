86e1396b7400928310c791b8055961b37579ea41
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-12 03:39:30 +0300

    Add files via upload

--- app/src/main/java/com/abueltaweel/presentation/screen/radio/components/RadioChannelsGrid.kt
+++ app/src/main/java/com/abueltaweel/presentation/screen/radio/components/RadioChannelsGrid.kt
@@ -1,25 +1,14 @@
 package com.abueltaweel.presentation.screen.radio.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyGridState
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.abueltaweel.design_system.theme.Theme
 import com.abueltaweel.presentation.components.LoadingContainer
-import com.abueltaweel.presentation.components.NoInternetContainer
 import com.abueltaweel.presentation.screen.radio.RadioChannelsViewModel
 import com.abueltaweel.presentation.screen.radio.RadioUiState
 
@@ -36,40 +25,19 @@ fun RadioChannelsGrid(
             .background(Theme.color.surfaces.surface)
             .windowInsetsPadding(WindowInsets.systemBars)
     ) {
-
-        when {
-
-            state.isLoading -> {
-                LoadingContainer(Modifier.align(Alignment.Center))
-            }
-
-            state.isNoInternet -> {
-                NoInternetContainer(
-                    onRetryClick = { viewModel.loadCategories() },
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
-
-            else -> {
-                LazyVerticalGrid(
-                    state = scrollState,
-                    modifier = modifier.fillMaxSize(),
-                    columns = GridCells.Adaptive(320.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    contentPadding = PaddingValues(
-                        horizontal = 16.dp,
-                        vertical = 8.dp
-                    )
-                ) {
-
-                    items(state.channels, key = { it.id }) {
-
-                        RadioReciterItem(
-                            state = it,
-                            listener = viewModel
-                        )
-                    }
+        if (state.isLoading) {
+            LoadingContainer(Modifier.align(Alignment.Center))
+        } else {
+            LazyVerticalGrid(
+                state = scrollState,
+                modifier = modifier.fillMaxSize(),
+                columns = GridCells.Adaptive(320.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+            ) {
+                items(state.channels, key = { it.id }) {
+                    RadioReciterItem(state = it, listener = viewModel)
                 }
             }
         }
