50efaf0ec1017b2b7844d60531d87395186e9c37
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-12 05:11:58 +0300

    Delete app/src/main/java/com/abueltaweel/presentation/screen/radio/RadioChannel.kt

--- app/src/main/java/com/abueltaweel/presentation/screen/radio/RadioChannel.kt
+++
@@ -1,8 +0,0 @@
-package com.abueltaweel.domain.entity.radio
-data class RadioChannel(
-    val id: Int,
-    val nameAr: String,
-    val nameEn: String,
-    val streamUrl: String,
-    val categoryId: String
-)
