1be5f4e1be91fb52f23365018ae70b50dfc54104
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-12 04:08:37 +0300

    Update RadioChannelsViewModel.kt

--- app/src/main/java/com/abueltaweel/presentation/screen/radio/RadioChannelsViewModel.kt
+++ app/src/main/java/com/abueltaweel/presentation/screen/radio/RadioChannelsViewModel.kt
@@ -7,18 +7,17 @@ import com.abueltaweel.presentation.screen.radio.player.PlayerState
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-// قنوات إسلامية ثابتة - مش محتاجة Supabase
 private val islamicRadioChannels = listOf(
-    RadioUiState.RadioChannelUiState(1,  "إذاعة القرآن الكريم المصرية",    "Egypt Quran Radio",      "http://stream.radiojar.com/0tpy1h0kxtzuv"),
-    RadioUiState.RadioChannelUiState(2,  "إذاعة القرآن الكريم السعودية",   "Saudi Quran Radio",      "http://stream.live.vc.bbcmedia.co.uk/bbc_arabic_radio"),
-    RadioUiState.RadioChannelUiState(3,  "إذاعة نداء الإسلام",             "Nida Al Islam",          "http://stream.radiojar.com/8s5u5tpdtwzuv"),
-    RadioUiState.RadioChannelUiState(4,  "إذاعة القرآن الكريم الكويت",     "Kuwait Quran Radio",     "http://www.radioislam.net:8100/listen.pls"),
-    RadioUiState.RadioChannelUiState(5,  "إذاعة المجد للقرآن",             "Al Majd Quran",          "http://stream.radiojar.com/4xgb4952n8zuv"),
-    RadioUiState.RadioChannelUiState(6,  "إذاعة القرآن الكريم المغرب",     "Morocco Quran Radio",    "http://stream.radiojar.com/0tpy1h0kxtzuv"),
-    RadioUiState.RadioChannelUiState(7,  "راديو إسلام",                    "Radio Islam",            "http://radioislam.net:8100"),
-    RadioUiState.RadioChannelUiState(8,  "إذاعة القرآن الكريم تونس",       "Tunisia Quran Radio",    "http://radiotunis.com:8000/quran.mp3"),
-    RadioUiState.RadioChannelUiState(9,  "إذاعة صوت الإسلام",              "Voice of Islam",         "http://stream.radiojar.com/8s5u5tpdtwzuv"),
-    RadioUiState.RadioChannelUiState(10, "إذاعة القرآن الكريم الجزائر",    "Algeria Quran Radio",    "http://stream.radiojar.com/4xgb4952n8zuv")
+    RadioUiState.RadioChannelUiState(1,  "إذاعة القرآن الكريم المصرية",  "Egypt Quran",    "http://stream.radiojar.com/0tpy1h0kxtzuv"),
+    RadioUiState.RadioChannelUiState(2,  "إذاعة القرآن الكريم السعودية", "Saudi Quran",    "https://n01.radiojar.com/ksa-quran.mp3"),
+    RadioUiState.RadioChannelUiState(3,  "إذاعة المجد للقرآن الكريم",    "Al Majd Quran",  "https://n01.radiojar.com/almajdquran.mp3"),
+    RadioUiState.RadioChannelUiState(4,  "إذاعة القرآن الكريم الكويت",   "Kuwait Quran",   "https://n01.radiojar.com/kuwaitquran.mp3"),
+    RadioUiState.RadioChannelUiState(5,  "إذاعة نداء الإسلام",           "Nida Al Islam",  "http://nadaalislamstream.com:8000/stream"),
+    RadioUiState.RadioChannelUiState(6,  "إذاعة القرآن الكريم المغرب",   "Morocco Quran",  "https://n01.radiojar.com/morocquran.mp3"),
+    RadioUiState.RadioChannelUiState(7,  "إذاعة القرآن الكريم الأردن",   "Jordan Quran",   "https://n01.radiojar.com/jordanquran.mp3"),
+    RadioUiState.RadioChannelUiState(8,  "إذاعة القرآن الكريم تونس",     "Tunisia Quran",  "https://n01.radiojar.com/tunisquran.mp3"),
+    RadioUiState.RadioChannelUiState(9,  "إذاعة القرآن الكريم الجزائر",  "Algeria Quran",  "https://n01.radiojar.com/algeriaQuran.mp3"),
+    RadioUiState.RadioChannelUiState(10, "راديو الإسلام",                 "Radio Islam",    "https://n01.radiojar.com/radioislam.mp3")
 )
 
 class RadioChannelsViewModel(
@@ -27,12 +26,8 @@ class RadioChannelsViewModel(
     RadioChannelsInteractionListener {
 
     init {
-        loadChannels()
-        observePlayerState()
-    }
-
-    private fun loadChannels() {
         updateState { it.copy(channels = islamicRadioChannels, isLoading = false) }
+        observePlayerState()
     }
 
     private fun observePlayerState() {
