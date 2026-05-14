e5f6ecb84ee705939554c21b129bbd69b6408557
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-14 11:53:39 +0300

    Update DhikrViewModel.kt

--- app/src/main/java/com/abueltaweel/presentation/screen/dhikr/DhikrViewModel.kt
+++ app/src/main/java/com/abueltaweel/presentation/screen/dhikr/DhikrViewModel.kt
@@ -40,7 +40,7 @@ val allDhikrs = listOf(
     DhikrItem(4, "لا حول ولا قوة إلا بالله",  R.raw.lahawla_wlaqowat),
     DhikrItem(5, "الصلاة على النبي",           R.raw.nozaker_salt_ala_habib),
     DhikrItem(6, "ربنا اغفر لي",              R.raw.rbna_ighfer_li),
-    DhikrItem(7, "سبحان الله وبحمده",          R.raw.sobhanallah_wabehamdeh)
+    DhikrItem(7, "سبحان الله وبحمده",          R.raw.sobhanallah_wabehamdeh), 
     DhikrItem(8, "الله أكبر",          R.raw.allah_akbar),
     DhikrItem(9, "أستغفر الله",        R.raw.astaghfer_allah),
     DhikrItem(10, "لا إله إلا الله",   R.raw.la_ilah_ela_allah),
