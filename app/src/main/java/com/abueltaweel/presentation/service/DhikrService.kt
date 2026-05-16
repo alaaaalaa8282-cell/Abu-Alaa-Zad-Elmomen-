080a3243842322cbaa3bb6eea0dbf99c625ab4d8
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-16 15:29:48 +0300

    Update DhikrService.kt

--- app/src/main/java/com/abueltaweel/presentation/service/DhikrService.kt
+++ app/src/main/java/com/abueltaweel/presentation/service/DhikrService.kt
@@ -83,7 +83,6 @@ class DhikrService : Service() {
         updateNotification(currentIndex)
 
         try {
-            mediaPlayer?.release()
             mediaPlayer = MediaPlayer.create(this, resId)?.apply {
                 setVolume(logVol, logVol)
                 setWakeMode(this@DhikrService, PowerManager.PARTIAL_WAKE_LOCK)
