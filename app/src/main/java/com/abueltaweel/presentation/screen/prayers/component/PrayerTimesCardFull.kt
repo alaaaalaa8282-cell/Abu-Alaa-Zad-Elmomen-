1dc2850c3d19200de2a9290ec3601431c30a121f
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-18 11:27:30 +0300

    Update PrayerTimesCardFull.kt

--- app/src/main/java/com/abueltaweel/presentation/screen/prayers/component/PrayerTimesCardFull.kt
+++ app/src/main/java/com/abueltaweel/presentation/screen/prayers/component/PrayerTimesCardFull.kt
@@ -1,8 +1,17 @@
-Enterpackage com.abueltaweel.presentation.screen.prayers.component
+package com.abueltaweel.presentation.screen.prayers.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -83,6 +92,7 @@ fun PrayerTimesCardFull(
     }
 }
 
+@OptIn(ExperimentalTime::class)
 @Composable
 private fun PrayerTimeItem(
     prayer: FullPrayerTimesUiState.PrayerUiState,
