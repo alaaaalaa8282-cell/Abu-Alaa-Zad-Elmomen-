4ccb34851116e9a9f34479ad23f4957449aa163c
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-18 05:38:23 +0300

    Create MosqueColors.kt

---
+++ app/src/main/java/com/abueltaweel/presentation/screen/prayers/MosqueColors.kt
@@ -0,0 +1,11 @@
+package com.abueltaweel.presentation.screen.prayers
+
+import androidx.compose.ui.graphics.Color
+
+object MosqueColors {
+    val Creamy  = Color(0xFFF5E6C0)
+    val Brown   = Color(0xFF6B2A0A)
+    val Gold    = Color(0xFFC9A84C)
+    val DarkBg  = Color(0xFF3D1A00)
+    val Border  = Color(0xFF8B4513)
+}
