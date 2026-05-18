8d620864df04af388f8de73c93eb64f71b4e906e
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-18 17:55:46 +0300

    Update Mappers.kt

--- app/src/main/java/com/abueltaweel/presentation/screen/prayers/Mappers.kt
+++ app/src/main/java/com/abueltaweel/presentation/screen/prayers/Mappers.kt
@@ -2,15 +2,13 @@ package com.abueltaweel.presentation.screen.prayers
 
 import com.abueltaweel.domain.entity.prayer.Prayer
 import com.abueltaweel.presentation.utils.format
-import com.abueltaweel.presentation.utils.toUiIcon
-import com.abueltaweel.presentation.utils.toUiName
 import kotlinx.datetime.TimeZone
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
 fun Prayer.toPrayerUiState(zone: TimeZone): FullPrayerTimesUiState.PrayerUiState {
     return FullPrayerTimesUiState.PrayerUiState(
-        name = this.toUiName(this.name),
+        name = this.name,  // استخدام الـ Enum مباشرة
         time = format(instant = this.time, zone = zone),
         icon = this.toUiIcon(this.name),
         instantTime = this.time
