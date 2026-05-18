e212d8579040539da4e894d66d31eccfc1d2a121
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-19 00:03:37 +0300

    Update Mappers.kt

--- app/src/main/java/com/abueltaweel/presentation/screen/prayers/Mappers.kt
+++ app/src/main/java/com/abueltaweel/presentation/screen/prayers/Mappers.kt
@@ -2,16 +2,14 @@ package com.abueltaweel.presentation.screen.prayers
 
 import com.abueltaweel.domain.entity.prayer.Prayer
 import com.abueltaweel.presentation.utils.format
-import com.abueltaweel.presentation.utils.toUiIcon // FIX: إضافة الاستيراد المفقود
 import kotlinx.datetime.TimeZone
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
 fun Prayer.toPrayerUiState(zone: TimeZone): FullPrayerTimesUiState.PrayerUiState {
     return FullPrayerTimesUiState.PrayerUiState(
-        name = this.name,
-        time = format(instant = this.time, zone = zone),
-        icon = this.toUiIcon(this.name),
+        name        = this.name,
+        time        = format(instant = this.time, zone = zone),
         instantTime = this.time
     )
 }
