9ad357e8b49251c3afbb7f7a64f84b740d9961ae
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-19 19:41:27 +0300

    Update LightThemeColors.kt

--- app/src/main/java/com/abueltaweel/design_system/color/LightThemeColors.kt
+++ app/src/main/java/com/abueltaweel/design_system/color/LightThemeColors.kt
@@ -9,11 +9,11 @@ val lightThemeColors: MehrabColors
         ),
         primary = Primary(
             primary = Color(0xFF000000),
-            onPrimary = Color(0xFF7D6A50),
+            onPrimary = Color(0xFFEFE5D0),
             shadePrimary = Color(0xFF0E1017),
         ), surfaces = Surfaces(
             surface = Color(0xFFF5EDD8),
-            surfaceLow = Color(0xFF7D6A50),
+            surfaceLow = Color(0xFFEFE5D0),
             surfaceHigh = Color(0xFFEDE0C4)
         ),
         secondary = Secondary(
