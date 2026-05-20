57ec8997c59a0cb225ebbed75a29e6323dc21c51
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-20 10:00:44 +0300

    Update HomeViewModel.kt

--- app/src/main/java/com/abueltaweel/presentation/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/abueltaweel/presentation/screen/home/HomeViewModel.kt
@@ -85,6 +85,8 @@ class HomeViewModel(
                 location = HomeUiState.LocationUiState(
                     country = location.country,
                     city = location.state
+                    latitude = location.latitude,   // أضف
+                   longitude = location.longitude  //
                 )
             )
         }
